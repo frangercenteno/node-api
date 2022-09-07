7faa57c2aca0910fddb04789aea854df83fb32eb
Franger Centeno <franger@crehana.com>
2022-09-06 21:44:19 -0500

    chore: install dependecies and create scaffold

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listen in port ${port}`);
+});
+
